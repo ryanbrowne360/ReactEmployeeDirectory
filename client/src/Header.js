2471cbf3f1be4ce1b56d848a1be3c4142ee94951
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-28 13:19:14 -0500

    styled header

--- client/src/Header.js
+++ client/src/Header.js
@@ -4,7 +4,7 @@ export default function Header(props) {
     
 
     return (
-        <div >
+        <div className="container-fluid text-center bg-primary pb-1 mb-2">
             <h1>Welcome to your Employee Directory!</h1>
             <h2>Use The Search Box To Find Any Person.</h2>
             <h3>Thank You For Using This Application.</h3>
