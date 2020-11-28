06fe5643b4a58deda16ce2dd1d47bd8bbaa40ac6
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-28 13:40:11 -0500

    changes made to GetEmployees componet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header";
-import GetEmployees  from ".GetEmployees"
+import GetEmployees  from "./GetEmployees"
 
 function App() {
   return (
--- client/src/GetEmployees.js
+++ client/src/GetEmployees.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
-export default function Getemployees(props) {
+export default function GetEmployees(props) {
 
     return (
         <>
           <div className="col-4 col-lg-2"></div>
           <div className="col-8 col-lg-3">
-            <input placeholder="Filter Name" {...props} />
+            <input placeholder="Search By Name" {...props} />
           </div>
           <div className="d-none d-lg-block col-lg-7"></div>
         </>
