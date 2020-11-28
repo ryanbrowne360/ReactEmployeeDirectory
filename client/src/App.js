8729276e96d3f7d0bdd515c916ea38707c238db6
ryanbrowne360 <ryanbrowne360@gmail.com>
2020-11-28 13:33:43 -0500

    getting search bar going

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./Header";
+import GetEmployees  from ".GetEmployees"
 
 function App() {
   return (
   
       <div>
     <Header />
+    <GetEmployees />
       </div>
    
   );
